d85709b7ab8be4d95d443163d94bb23917391c77
1 <1@303-3>
2020-03-04 17:33:35 +0500

    Форма логина

--- esoft_2/esoft_2/Form1.Designer.cs
+++ esoft_2/esoft_2/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 146);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 230);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(241, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 146);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 230);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 369);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Вход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(507, 500);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Форма входа";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- esoft_2/esoft_2/Form1.cs
+++ esoft_2/esoft_2/Form1.cs
@@ -7,14 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
 
 namespace esoft_2
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection("Data Source = 303-3\\SQLEXPRESS Initial Catalog = esoft_2; Integrated Security = true;");
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com_man = new SqlCommand("SELECT * FROM dbo.managers WHERE Логин_менеджера = '" + textBox1.Text + "' AND Пароль_менеджера = '" + textBox2.Text + "'", con);
+            //SqlCommand com_usr = new SqlCommand($"SELECT id FROM users WHERE Логин_исполнителя = '" + textBox1.Text + "' AND Пароль_исполнителя = '" + textBox2.Text + "'", con);
+            SqlDataReader drman = com_man.ExecuteReader();
+            if (drman.HasRows)
+            {
+                drman.Read();
+                MessageBox.Show("Авторизация как менеджер");
+                Form2 f2 = new Form2();
+                f2.Show();
+                this.Hide();
+            }
+            //else
+            //{
+            //    drman.Close();
+            //    SqlDataReader drusr = com_usr.ExecuteReader();
+            //    if (drusr.HasRows) MessageBox.Show("Вы вошли как пользователь");
+            //    else MessageBox.Show("Неверный логин или пароль!");
+            //    drusr.Close();
+            //}
+            con.Close();
         }
     }
 }
